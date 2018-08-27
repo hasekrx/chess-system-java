c34dceca29be797dd53f5edd5ee79813fe8f05a3
Alexandre Carriconde <alexandre_carriconde@qat.com>
2018-08-27 13:21:44 -0400

    ChessException and ChessPosition

---
+++ src/chess/ChessExpection.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessExpection extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ChessExpection(String msg) {
+		super(msg);
+	}
+
+}
